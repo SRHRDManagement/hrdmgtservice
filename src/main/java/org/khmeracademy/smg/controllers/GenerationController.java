ec43ec736aa8ee68c1626669114931f3cfb6139b
vansa007 <vs.lov.rs@gmail.com>
2016-08-03 10:18:54 +0700

    test control

--- src/main/java/org/khmeracademy/smg/controllers/GenerationController.java
+++ src/main/java/org/khmeracademy/smg/controllers/GenerationController.java
@@ -36,7 +36,7 @@ public class GenerationController {
 				map.put("STATUS", false);
 			}
 		} catch (Exception e) {
-			map.put("MESSAGE", "Error! hasha");
+			map.put("MESSAGE", "Error! hasha love");
 			map.put("STATUS", false);
 			e.printStackTrace();
 		}
