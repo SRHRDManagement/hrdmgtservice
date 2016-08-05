466ee760e8043c4a1ee017d391f4229c0e884296
vansa007 <vs.lov.rs@gmail.com>
2016-08-05 12:04:31 +0700

    today

--- src/main/java/org/khmeracademy/smg/controllers/rest/GenerationRestController.java
+++
@@ -1,5 +0,0 @@
-package org.khmeracademy.smg.controllers.rest;
-
-public class GenerationRestController {
-
-}
--- src/main/java/org/khmeracademy/smg/services/CourseService.java
+++
@@ -1,7 +0,0 @@
-package org.khmeracademy.smg.services;
-
-import org.khmeracademy.smg.model.Course;
-
-public interface CourseService {
-	public boolean addCourse(Course course);
-}
